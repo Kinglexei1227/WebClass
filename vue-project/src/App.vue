e719afc6ebbeaf49649b77113b961ea51610e779
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-07-08 14:33:47 +0900

    vues

---
+++ vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import ExpressTest from './components/ExpressTest.vue'
+
+export default {
+  components: {
+    ExpressTest
+  }
+}
+</script>
+
+<template>
+  <div>
+    <ExpressTest />
+  </div>
+</template>
