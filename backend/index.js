0ee95927ef6e77ce463d5f35b164ef69e2941a47
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-07-08 14:32:46 +0900

    index

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,32 @@
-// backend/index.js
+const mysql = require('mysql2');
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 const PORT = 3000;
 
-// CORS 설정
 app.use(cors());
+app.use(express.json()); // JSON 요청 해석
 
-// GET 요청 응답
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from Express!' });
+// DB 연결
+const conn = mysql.createConnection({
+    host: 'DB주소',
+    user: 'DB유저',
+    password: 'DB비밀번호',
+    database: 'DB이름'
 });
 
-// 서버 실행
+// 데이터 등록
+app.post('/api/register', (req, res) => {
+    const { username, password } = req.body;
+
+    const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    conn.query(sql, [username, password], (err, result) => {
+        if (err) return res.status(500).send('DB 저장 실패');
+        res.send('회원가입 성공');
+    });
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
